b2f3af2a15da0c1a007d0ebba7c3d653dd7ed923
Jeremiah <jscott4720@gmail.com>
2020-07-16 16:22:00 -0400

    added hover to buttons

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,5 +3,37 @@ import { storiesOf } from '@storybook/react';
 
 import Button from './Button';
 
+
+
 storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+    .add('Primary', () => <Button type = 'primary' label="Primary" />) 
+    .add('Success', () => <Button type="success"  label="Success" />) 
+    .add('Danger', () => <Button type='danger' label="Danger" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Primary Large', () => <Button type='primary' large label="Primary Large" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+    .add('Warning', () => <Button type='warning' label="Warning" />) 
+
+    
+   
+    
+
+    
